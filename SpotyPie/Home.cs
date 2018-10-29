3ddcdf59008042a76a1f03bc41ddeafde66ac324
Eimantas Noreika <noreika.eimantas@gmail.com>
2018-10-29 13:26:03 +0200

    Binded recycle view with home fragment

--- SpotyPie/Home.cs
+++ SpotyPie/Home.cs
@@ -23,35 +23,95 @@ namespace SpotyPie
     {
         View RootView;
 
+        //Recent albums
         public static RecycleViewList<string> RecentAlbums = new RecycleViewList<string>();
-        private RecyclerView.LayoutManager mLayoutManager;
-        private static RecyclerView.Adapter mAdapter;
-        private static RecyclerView mRecyclerView;
+        private RecyclerView.LayoutManager RecentAlbumsLayoutManager;
+        private static RecyclerView.Adapter RecentAlbumsAdapter;
+        private static RecyclerView RecentAlbumsRecyclerView;
+
+        //Best albums
+        public static RecycleViewList<string> BestAlbums = new RecycleViewList<string>();
+        private RecyclerView.LayoutManager BestAlbumsLayoutManager;
+        private static RecyclerView.Adapter BestAlbumsAdapter;
+        private static RecyclerView BestAlbumsRecyclerView;
+
+        //Jump back albums
+        public static RecycleViewList<string> JumpBack = new RecycleViewList<string>();
+        private RecyclerView.LayoutManager JumpBackLayoutManager;
+        private static RecyclerView.Adapter JumpBackAdapter;
+        private static RecyclerView JumpBackRecyclerView;
+
+        //Top playlist
+        public static RecycleViewList<string> TopPlaylist = new RecycleViewList<string>();
+        private RecyclerView.LayoutManager TopPlaylistLayoutManager;
+        private static RecyclerView.Adapter TopPlaylistAdapter;
+        private static RecyclerView TopPlaylistRecyclerView;
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             RootView = inflater.Inflate(Resource.Layout.home_layout, container, false);
 
-            mLayoutManager = new LinearLayoutManager(this.Activity, LinearLayoutManager.Horizontal, false);
-            mRecyclerView = RootView.FindViewById<RecyclerView>(Resource.Id.recent_rv);
-            mRecyclerView.SetLayoutManager(mLayoutManager);
-            mAdapter = new HorizontalRV(RecentAlbums, mRecyclerView);
-            RecentAlbums.Adapter = mAdapter;
-            mRecyclerView.SetAdapter(mAdapter);
+            RecentAlbumsLayoutManager = new LinearLayoutManager(this.Activity, LinearLayoutManager.Horizontal, false);
+            RecentAlbumsRecyclerView = RootView.FindViewById<RecyclerView>(Resource.Id.recent_rv);
+            RecentAlbumsRecyclerView.SetLayoutManager(RecentAlbumsLayoutManager);
+            RecentAlbumsAdapter = new HorizontalRV(RecentAlbums, RecentAlbumsRecyclerView);
+            RecentAlbums.Adapter = RecentAlbumsAdapter;
+            RecentAlbumsRecyclerView.SetAdapter(RecentAlbumsAdapter);
 
             RecentAlbums.Add("das");
             RecentAlbums.Add("das");
             RecentAlbums.Add("das");
-            RecentAlbums.Add("das");
-            RecentAlbums.Add("das");
-            RecentAlbums.Add("das");
-            RecentAlbums.Add("das");
-            RecentAlbums.Add("das");
-            RecentAlbums.Add("das");
-            RecentAlbums.Add("das");
-            RecentAlbums.Add("das");
+
+
+            BestAlbumsLayoutManager = new LinearLayoutManager(this.Activity, LinearLayoutManager.Horizontal, false);
+            BestAlbumsRecyclerView = RootView.FindViewById<RecyclerView>(Resource.Id.best_artists_rv);
+            BestAlbumsRecyclerView.SetLayoutManager(BestAlbumsLayoutManager);
+            BestAlbumsAdapter = new HorizontalRV(BestAlbums, BestAlbumsRecyclerView);
+            BestAlbums.Adapter = BestAlbumsAdapter;
+            BestAlbumsRecyclerView.SetAdapter(BestAlbumsAdapter);
+
+            BestAlbums.Add("das");
+            BestAlbums.Add("das");
+            BestAlbums.Add("das");
+
+
+            JumpBackLayoutManager = new LinearLayoutManager(this.Activity, LinearLayoutManager.Horizontal, false);
+            JumpBackRecyclerView = RootView.FindViewById<RecyclerView>(Resource.Id.albums_old_rv);
+            BestAlbumsRecyclerView.SetLayoutManager(JumpBackLayoutManager);
+            JumpBackAdapter = new HorizontalRV(JumpBack, JumpBackRecyclerView);
+            JumpBack.Adapter = JumpBackAdapter;
+            JumpBackRecyclerView.SetAdapter(JumpBackAdapter);
+
+            JumpBack.Add("das");
+            JumpBack.Add("das");
+            JumpBack.Add("das");
+
+
+            TopPlaylistLayoutManager = new LinearLayoutManager(this.Activity, LinearLayoutManager.Horizontal, false);
+            TopPlaylistRecyclerView = RootView.FindViewById<RecyclerView>(Resource.Id.playlist_rv);
+            BestAlbumsRecyclerView.SetLayoutManager(TopPlaylistLayoutManager);
+            TopPlaylistAdapter = new HorizontalRV(TopPlaylist, TopPlaylistRecyclerView);
+            TopPlaylist.Adapter = TopPlaylistAdapter;
+            TopPlaylistRecyclerView.SetAdapter(TopPlaylistAdapter);
+
+            TopPlaylist.Add("das");
+            TopPlaylist.Add("das");
+            TopPlaylist.Add("das");
+
 
             return RootView;
+        }
+
+        public void GetRecentAlbums()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+
+            }
         }
     }
 }
