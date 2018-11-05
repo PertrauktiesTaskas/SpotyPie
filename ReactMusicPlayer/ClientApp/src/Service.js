eeb1ae700b2e91c625561a4b6e4df5d1ca29e0ae
Renaldas Štilpa <renaldas.stilpa@gmail.com>
2018-11-05 15:33:50 +0200

    Added services to React page

---
+++ ReactMusicPlayer/ClientApp/src/Service.js
@@ -0,0 +1,45 @@
+const apiEnd = 'http://spotypie.deveim.com/api';
+
+export const itemService = {
+    getAlbums,
+    getArtists,
+    getSongs
+};
+
+function getAlbums() {
+    const requestOptions = {
+        method: 'GET'
+    };
+    return fetch(apiEnd + '/Albums', requestOptions)
+        .then(handleResponse);
+}
+
+function getArtists(){
+    const requestOptions = {
+        method: 'GET'
+    };
+    return fetch(apiEnd + '/Artists', requestOptions)
+        .then(handleResponse);
+}
+
+function getSongs(){
+    const requestOptions = {
+        method: 'GET'
+    };
+    return fetch(apiEnd + '/Songs', requestOptions)
+        .then(handleResponse);
+}
+
+function handleResponse(response) {
+    return response.text().then(data => {
+            if (response.ok) {
+                let parsed = data && JSON.parse(data);
+                return parsed
+            }
+            return undefined;
+        },
+        error => {
+            console.log(error, 'handleResponse error');
+            return error;
+        });
+}
