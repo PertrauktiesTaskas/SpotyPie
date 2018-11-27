cf132a63930f74a14804150c5870f53d401a45c2
Eimantas Noreika <noreika.eimantas@gmail.com>
2018-11-27 16:17:39 +0200

    Created player song list fragment and view.

--- SpotyPie/Player/player_song_list.cs
+++ SpotyPie/Player/player_song_list.cs
@@ -1,8 +1,5 @@
 ﻿using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Support.Constraints;
-using Android.Support.V4.Widget;
 using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
@@ -10,30 +7,17 @@ using Newtonsoft.Json;
 using RestSharp;
 using SpotyPie.Helpers;
 using SpotyPie.Models;
-using Square.Picasso;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using static Android.Views.View;
-using static Android.Views.ViewGroup;
 using SupportFragment = Android.Support.V4.App.Fragment;
 
-namespace SpotyPie
+namespace SpotyPie.Player
 {
-    public class AlbumFragment : SupportFragment
+    public class player_song_list : SupportFragment
     {
         View RootView;
-
-        //Background info
-
-        ImageView AlbumPhoto;
-        TextView AlbumTitle;
-        Button PlayableButton;
-        TextView AlbumByText;
-
-        TextView ButtonBackGround;
-        TextView ButtonBackGround2;
 
         //Album Songs
         public static List<Item> AlbumSongsItem = new List<Item>();
@@ -42,49 +26,9 @@ namespace SpotyPie
         private static RecyclerView.Adapter AlbumSongsAdapter;
         private static RecyclerView AlbumSongsRecyclerView;
 
-        private TextView download;
-        private TextView Copyrights;
-        private ConstraintLayout backViewContainer;
-        int Height = 0;
-
-        MarginLayoutParams MarginParrams;
-        RelativeLayout relative;
-
-        private NestedScrollView Scroll;
-        private NestedScrollView ScrollFather;
-        int scrolled = 0;
-
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            RootView = inflater.Inflate(Resource.Layout.Album_layout, container, false);
-
-            MainActivity.ActionName.Text = Current_state.ClickedInRVH.Title;
-
-            //Background binding
-            AlbumPhoto = RootView.FindViewById<ImageView>(Resource.Id.album_photo);
-            AlbumTitle = RootView.FindViewById<TextView>(Resource.Id.album_title);
-            PlayableButton = RootView.FindViewById<Button>(Resource.Id.playable_button);
-            AlbumByText = RootView.FindViewById<TextView>(Resource.Id.album_by_title);
-
-            ButtonBackGround = RootView.FindViewById<TextView>(Resource.Id.backgroundHalf);
-            ButtonBackGround2 = RootView.FindViewById<TextView>(Resource.Id.backgroundHalfInner);
-
-            Picasso.With(Context).Load(Current_state.ClickedInRVH.Image).Into(AlbumPhoto);
-            AlbumTitle.Text = Current_state.ClickedInRVH.Title;
-            AlbumByText.Text = Current_state.ClickedInRVH.SubTitle;
-
-            Current_state.ShowHeaderNavigationButtons();
-
-            download = RootView.FindViewById<TextView>(Resource.Id.download_text);
-            Copyrights = RootView.FindViewById<TextView>(Resource.Id.copyrights);
-            MarginParrams = (MarginLayoutParams)download.LayoutParameters;
-
-            relative = RootView.FindViewById<RelativeLayout>(Resource.Id.hide);
-
-            ScrollFather = RootView.FindViewById<NestedScrollView>(Resource.Id.fatherScrool);
-            backViewContainer = RootView.FindViewById<ConstraintLayout>(Resource.Id.backViewContainer);
-            Height = backViewContainer.LayoutParameters.Height;
-            ScrollFather.ScrollChange += Scroll_ScrollChange;
+            RootView = inflater.Inflate(Resource.Layout.player_song_list, container, false);
 
             //ALBUM song list
             AlbumSongsLayoutManager = new LinearLayoutManager(this.Activity);
@@ -100,16 +44,11 @@ namespace SpotyPie
                 if (AlbumSongsRecyclerView != null && AlbumSongsRecyclerView.ChildCount != 0)
                 {
                     Current_state.SetSong(Current_state.Current_Song_List[position]);
+                    Player.PlayerSongListContainer.TranslationX = MainActivity.widthInDp;
                 }
             });
 
             return RootView;
-        }
-
-        public override void OnDestroyView()
-        {
-            AlbumSongs = new RecycleViewList<List>();
-            base.OnDestroyView();
         }
 
         public override void OnResume()
@@ -139,7 +78,7 @@ namespace SpotyPie
                             AlbumSongs.Add(new List(x.Id, x.Name, JsonConvert.DeserializeObject<List<Artist>>(x.Artists).First().Name));
                         }
                         List<Copyright> Copyright = JsonConvert.DeserializeObject<List<Copyright>>(album.Copyrights);
-                        Copyrights.Text = string.Join("\n", Copyright.Select(x => x.Text));
+                        //Copyrights.Text = string.Join("\n", Copyright.Select(x => x.Text));
                     }, null);
                 }
                 else
@@ -156,19 +95,10 @@ namespace SpotyPie
             }
         }
 
-        private void Scroll_ScrollChange(object sender, NestedScrollView.ScrollChangeEventArgs e)
+        public override void OnDestroyView()
         {
-            scrolled = ScrollFather.ScrollY;
-            if (scrolled < Height) //761 mazdaug
-            {
-                MainActivity.ActionName.Alpha = (float)((scrolled * 100) / Height) / 100;
-                ButtonBackGround.Alpha = (float)((scrolled * 100) / Height) / 100;
-                relative.Visibility = ViewStates.Invisible;
-            }
-            else
-            {
-                relative.Visibility = ViewStates.Visible;
-            }
+            AlbumSongs = new RecycleViewList<List>();
+            base.OnDestroyView();
         }
     }
 }
